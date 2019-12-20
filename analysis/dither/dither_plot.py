b76d1f792f0f4354ed24bfe4786f45cfc1de6f9e
Tanveer <tanveer.karim@cfa.harvard.edu>
2019-12-20 01:29:17 -0700

    fix bugs and indentation errors

--- analysis/dither/dither_plot.py
+++ analysis/dither/dither_plot.py
@@ -1,3 +1,9 @@
+"""This is the version of dither_plot.py that works. Old one
+has been saved as dither_plot_old.py
+
+Debugged by: Tanveer Karim & Dragan Huterer
+Date: 18 Dec 2019"""
+
 import csv,sys,os
 import argparse
 from math import sqrt
@@ -12,95 +18,96 @@ from matplotlib import gridspec
    
 
 def grokdither(seqlist, obsday, petalnum, channel, fiberassigncsvfilename, nightwatchroot='/exposures/nightwatch/', figoutdir='/n/home/desiobserver/dithertest/'):
-   petal_assign = {}   
-   with open(fiberassigncsvfilename) as dithers:
-      
-      reader = csv.DictReader(dithers, skipinitialspace=True)
-      for line in reader:
-         if line['PETAL'] == petalnum:
+    petal_assign = {}   
+    with open(fiberassigncsvfilename) as dithers:
+        
+        reader = csv.DictReader(dithers, skipinitialspace=True)
+        
+        for line in reader:
+            if line['PETAL'] == petalnum:
             
-            fiber = int(line['FIBER'])
-            x = float(line['OROGINALLY_ASSINED_FIBER_X'])
-            y = float(line['ORIGINALLY_ASSIGNED_FIBER_Y'])
-            dra  = float(line['DELTA_RA_DITHERED_ORIGINAL_degrees'])*3600
-            ddec = float(line['DELTA_DEC_DITHERED_ORIGINAL_degrees'])*3600
-            if dra*dra+ddec*ddec<225:
-               petal_assign[fiber] = {}
-               
-               petal_assign[fiber]['r'] = sqrt(x*x+y*y)
-               
-               petal_assign[fiber]['inner'] = petal_assign[fiber]['r']<285
-               
-               petal_assign[fiber]['dra'] = dra
-               
-               petal_assign[fiber]['ddec'] = ddec
-               
-               
-      for expid in seqlist:
+                fiber = int(line['FIBER'])
+                x = float(line['OROGINALLY_ASSINED_FIBER_X'])
+                y = float(line['ORIGINALLY_ASSIGNED_FIBER_Y'])
+                dra  = float(line['DELTA_RA_DITHERED_ORIGINAL_degrees'])*3600
+                ddec = float(line['DELTA_DEC_DITHERED_ORIGINAL_degrees'])*3600
+            
+                if dra*dra+ddec*ddec<225:
+                    petal_assign[fiber] = {}
 
-         nightwatch_filename  =   nightwatchroot + obsday + '/000'+str(expid)+'/qa-000'+str(expid)+'.fits'
-         output_filename = 'plot'+str(expid)+'.png'
-         
-         nw = fitsio.FITS(nightwatch_filename)
-         data = nw['PER_CAMFIBER'].read()
-         fibers = data['FIBER']
-         snrs = data['MEDIAN_CALIB_SNR']
-         cams = data['CAM']
-         for fiber, snr, cam in zip(fibers, snrs, cams):
-            #print(str(fiber)+' '+str(snr)+' '+str(cam))
-            if cam == channel and fiber in petal_assign:
-               camera=channel
-               petal_assign[fiber]['snr'] = snr
+                    petal_assign[fiber]['r'] = sqrt(x*x+y*y)
+
+                    petal_assign[fiber]['inner'] = petal_assign[fiber]['r']<285
+
+                    petal_assign[fiber]['dra'] = dra
+
+                    petal_assign[fiber]['ddec'] = ddec
                
-         #print(foo)
-         df = DataFrame.from_dict(petal_assign, orient='index')
-         if ((df[df['snr']>0.5].size >0) & (df[df['snr']>0.5][df['r']<225].size >0)) :
-            plot_title = obsday+' - Exp '+str(expid)+'- PETAL_LOC '+petalnum+' Cam: '+str(camera.decode())
-            ii = (np.abs(df['dra']) > 3.5* np.std(df['dra'])) |  (np.abs(df['ddec']) > 3.5* np.std(df['ddec']))
-            df = df[~ii]
-            
-            ax = df.plot(kind='scatter', x='dra', y='ddec', s=3, color='gray')
-            df[df['snr']>0.5].plot(kind='scatter', x='dra', y='ddec', color='blue', ax=ax)
-            df[df['snr']>.5][df['r']<225].plot(kind='scatter', x='dra', y='ddec', color='red', ax=ax)
-            
-            xlim = (np.min(df['dra'])-0.5,np.max(df['dra'])+0.5 )
-            ylim = (np.min(df['ddec'])-0.5,np.max(df['ddec'])+0.5 )
-            
-            ax.set_xlim(xlim)
-            ax.set_ylim(ylim)
-            ax.set_title(plot_title)
-            ax.set_xlabel('delta ra, arcsec')
-            ax.set_ylabel('delta dec, arcsec')
-            ax.minorticks_on()
-            ax.tick_params(which='major', length=8, width=1.5, direction='in') 
-            ax.tick_params(which='minor', length=6, color='#000033', width=1.0, direction='in')        
-            x_ax = ax.twiny()
-            x_ax.minorticks_on()
-            x_ax.tick_params(which='major', length=8, width=1.0, direction='in')
-            x_ax.tick_params(which='minor', length=6, color='#000033', width=1.0, direction='in')     
-            x_ax.tick_params()
-            y_ax= plt.twinx()
-            y_ax.minorticks_on() 
-            x_ax.set_xlim(xlim)
-            y_ax.set_ylim(ylim)
-            y_ax.tick_params(which='major', length=8, width=1.0, direction='in')
-            y_ax.tick_params(which='minor', length=6, color='#000033', width=1.0, direction='in')
-            py.savefig(figoutdir+output_filename)
-         else:                  
-            print('None of the fibers in '+'qa-000'+str(expid)+'.fits'+' have landed on a source (snr >0.5) in petal '+petalnum)
-            continue
-         plt.show()
+               
+        for expid in seqlist:
+            nightwatch_filename  =   nightwatchroot + obsday + '/000'+str(expid)+'/qa-000'+str(expid)+'.fits'
+            output_filename = 'plot'+str(expid)+'.png'
+         
+            nw = fitsio.FITS(nightwatch_filename)
+            data = nw['PER_CAMFIBER'].read()
+            fibers = data['FIBER']
+            snrs = data['MEDIAN_CALIB_SNR']
+            cams = data['CAM']
+         
+            for fiber, snr, cam in zip(fibers, snrs, cams):
+                #print(str(fiber)+' '+str(snr)+' '+str(cam))
+                if cam == channel and fiber in petal_assign:
+                    camera=channel
+                    #print(type(camera))
+                    #print(camera)
+                    petal_assign[fiber]['snr'] = snr
+               
+             #print(foo)
+            df = DataFrame.from_dict(petal_assign, orient='index')
+            #print(df.columns)
+            if ((df[df['snr']>0.5].size >0) & (df[df['snr']>0.5][df['r']<225].size >0)) :
+                plot_title = obsday+' - Exp '+str(expid)+'- PETAL_LOC '+petalnum+' Cam: '+str(camera)
+                ii = (np.abs(df['dra']) > 3.5* np.std(df['dra'])) |  (np.abs(df['ddec']) > 3.5* np.std(df['ddec']))
+                df = df[~ii]
+
+                ax = df.plot(kind='scatter', x='dra', y='ddec', s=3, color='gray')
+                df[df['snr']>0.5].plot(kind='scatter', x='dra', y='ddec', color='blue', ax=ax)
+                df[df['snr']>.5][df['r']<225].plot(kind='scatter', x='dra', y='ddec', color='red', ax=ax)
+
+                xlim = (np.min(df['dra'])-0.5,np.max(df['dra'])+0.5 )
+                ylim = (np.min(df['ddec'])-0.5,np.max(df['ddec'])+0.5 )
+
+                ax.set_xlim(xlim)
+                ax.set_ylim(ylim)
+                ax.set_title(plot_title)
+                ax.set_xlabel('delta ra, arcsec')
+                ax.set_ylabel('delta dec, arcsec')
+                ax.minorticks_on()
+                ax.tick_params(which='major', length=8, width=1.5, direction='in') 
+                ax.tick_params(which='minor', length=6, color='#000033', width=1.0, direction='in')        
+                x_ax = ax.twiny()
+                x_ax.minorticks_on()
+                x_ax.tick_params(which='major', length=8, width=1.0, direction='in')
+                x_ax.tick_params(which='minor', length=6, color='#000033', width=1.0, direction='in')     
+                x_ax.tick_params()
+                y_ax= plt.twinx()
+                y_ax.minorticks_on() 
+                x_ax.set_xlim(xlim)
+                y_ax.set_ylim(ylim)
+                y_ax.tick_params(which='major', length=8, width=1.0, direction='in')
+                y_ax.tick_params(which='minor', length=6, color='#000033', width=1.0, direction='in')
+                py.savefig(figoutdir+output_filename)
+            else:                  
+                print('None of the fibers in '+'qa-000'+str(expid)+'.fits'+' have landed on a source (snr >0.5) in petal '+petalnum)
+                continue
+            plt.show()
                   
 
 
 
 
 def plot_dither_seq(exposure_sequence, obsday, petalnum_list, channel, tileid, nightwatchdir,fiberassign_dir, plotout_dir,snr_thresh):
-        
-        
-    
-
-     '''NOTE: this hack to replace the csv file with fiberassign's raw output 
+    '''NOTE: this hack to replace the csv file with fiberassign's raw output 
               and it only works if the originally assigned tile is also supplied.
               For all the dithered tiles that are provided (see ), 
               the originally assigned tile is supplied and its tile ID is one 
@@ -123,8 +130,8 @@ def plot_dither_seq(exposure_sequence, obsday, petalnum_list, channel, tileid, n
 
         
         
-    print('original assignment is: '+fiberassign_dir+"/fiberassign-0{}.fits".format(str(tileid-1)))
-    print('Dithered assignment is: '+fiberassign_dir+"/fiberassign-0{}.fits".format(str(tileid)))
+    print('original assignment is: '+fiberassign_dir+"fiberassign-0{}.fits".format(str(tileid-1)))
+    print('Dithered assignment is: '+fiberassign_dir+"fiberassign-0{}.fits".format(str(tileid)))
     print('if these are not the correct files to be used, stop now!')
                 
    
@@ -182,11 +189,11 @@ def plot_dither_seq(exposure_sequence, obsday, petalnum_list, channel, tileid, n
 
                 df = DataFrame.from_dict(petal_assign, orient='index')
                 ax = plt.subplot(gs[ip]); ip += 1
-                plot_title = obsday+' - Exp '+str(expid)+'- PETAL_LOC '+str(petalnum)+' Cam: '+str(camera.decode())
+                plot_title = obsday+' - Exp '+str(expid)+'- PETAL_LOC '+str(petalnum)+' Cam: '+str(camera)
                 ax.set_title(plot_title, fontsize=15)
 
                 if ((df[df['snr']>snr_thresh].size >0) & (df[df['snr']>snr_thresh][df['r']<225].size >0)) :
-                    plot_title = obsday+' - Exp '+str(expid)+'- PETAL_LOC '+str(petalnum)+' Cam: '+str(camera.decode())
+                    plot_title = obsday+' - Exp '+str(expid)+'- PETAL_LOC '+str(petalnum)+' Cam: '+str(camera)
                     ii = (np.abs(df['dra']) > 3.5* np.std(df['dra'])) |  (np.abs(df['ddec']) > 3.5* np.std(df['ddec']))
                     df = df[~ii]
 
@@ -237,11 +244,12 @@ def plot_dither_seq(exposure_sequence, obsday, petalnum_list, channel, tileid, n
 
                     continue
                     
-        pylab.savefig(plotout_dir+output_filename)
+        py.savefig(plotout_dir+output_filename)
 
         plt.show()
         print('_________________________________________________________________________________')  
         print('')
+
 
 
 
